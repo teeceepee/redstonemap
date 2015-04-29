e3116a5c12bb78a3507308c4a9331bfb642d799d
Yu Haidong <yuhaidongyhd@gmail.com>
2015-04-29 22:19:16 +0800

    Add company index pagination

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,7 +4,7 @@ class CompaniesController < ApplicationController
   # GET /companies
   # GET /companies.json
   def index
-    @companies = Company.all
+    @companies = Company.all.page(params[:page]).per(4)
   end
 
   # GET /companies/1
